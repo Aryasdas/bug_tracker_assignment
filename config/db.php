5e0e6cd990852c6405b352d4001a44bed0ef64eb
Arya Sundar Das <140619090+Aryasdas@users.noreply.github.com>
2025-06-20 16:48:44 +0530

    Add files via upload
    Database configuration and data base connection of this  project

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'localhost';
+$db   = 'bug_tracker';
+$user = 'root';
+$pass = '';
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
